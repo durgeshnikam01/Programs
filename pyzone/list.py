e343afc1795e40762e1048e1e5f36e2b1606a756
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-09 14:34:23 +0530

    list operations

---
+++ pyzone/list.py
@@ -0,0 +1,46 @@
+#List starts with 0 index place ,can add and remove ites from it.
+# my_List=["apple","Banaba","Kiwi","graps","Mango"]
+# print(my_List)
+# print(len(my_List))
+
+
+#LIST CAN CONTAIN ALL DATA TYPES ELEMENTS
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+# print(my_List[-1])
+# print(my_List[2:4])
+
+
+# myList=['apple',"banana","cherry"]
+# myList[0]="kiwi"
+# print(myList[1])
+# print(myList)
+
+
+# mylist=["apple","banaba","kiwi","gwava"]
+# # mylist[1:2]=["greps","mango"]
+# # print(mylist)
+
+# #ADDING LIST ITEMS
+# mylist.insert(0,"tomato")
+
+# print(mylist[3])
+# #ADDING THE ITEMS IN  LIST
+# mylist.append("orange")
+# print(mylist)
+# #ADDING ITEM AT SECOND PLACE
+# mylist.insert(1,"lemon")
+# print(mylist)
+
+# TO JOIN THE TWO LISTS
+# tropical=["mango","pineapple","papaya"]
+# fruits.extend(tropical)
+
+#remove items
+# fruits.remove("banana")
+#pop
+fruits=["apple","banana","cherry"]
+fruits.pop(1)
