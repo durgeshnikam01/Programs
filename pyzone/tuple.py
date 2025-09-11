cec82bfb883f02bee8f358597bf67ef4a6605922
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-11 15:01:47 +0530

    loop in tuple

--- pyzone/tuple.py
+++ pyzone/tuple.py
@@ -32,7 +32,70 @@
 # pro="profession:",info_person[2]
 # print(pro)
 
-animals = ("dog", "cat", "rabbit")
-for x in animals:
-    print(x)
-print(x)
+# animals = ("dog", "cat", "rabbit")
+# for x in animals:
+#   print(x)
+
+# t1 = (1, 2, 3)
+# t2 = (4, 5, 6)
+# t3=t1+t2
+# print(t3)
+
+# n="hi"
+# for x in n:
+#     print(n * 4)
+#     break
+
+# fruits = ("apple", "banana", "apple", "cherry", "apple")
+# print(fruits.count("apple"))
+
+# nums = (10, 20, 30, 40, 50)
+# print(nums.index(30))
+
+# fruits = ("apple", "banana", "cherry",)
+# if "mango" in fruits:
+#     print("True")
+# else:
+#     print("False")
+# nums = (5, 10, 15, 20, 25)
+# list_nums=list(nums)
+# print(type(list_nums))
+# print(list_nums[1:4])
+# nums=tuple(list_nums)
+# print(type(nums))
+
+# colors = ("red", "green", "blue")
+# print(type(colors))
+# list_color=list(colors)
+# list_color[1]="yellow"
+# print(list_color)
+# colors=tuple(list_color)
+# print(type(colors))
+
+
+# laptop = ("HP", "i5", 8, "Windows 11")
+# print(f"brand:",laptop[0])
+# print(f"processer:",laptop[1])
+# print(f"Ram:",laptop[2])
+# print(f"OS:",laptop[3])
+
+# fruits = ("apple", "banana", "cherry", "banana")
+# print(fruits.index("banana"))
+
+# animals = ("dog", "cat", "rabbit", "parrot")
+# for animal in animals:
+#     print(animal)
+
+# nums = (10, 20, 30, 40, 50)
+# for num in nums:
+#     print(num*2)
+#     num+1
+
+# marks = (78, 55, 92, 67, 88)
+# for mark in marks:
+#     if mark > 70:
+#         print(mark)
+
+# colors = ("red", "green", "blue", "yellow")
+# # for color in colors:
+# print(enumerate(colors))
