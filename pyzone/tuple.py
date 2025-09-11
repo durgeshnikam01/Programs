ad80204e011c84f59d498b5f2bcb2c1d39237c22
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-11 10:52:38 +0530

    Tuple operations

---
+++ pyzone/tuple.py
@@ -0,0 +1,38 @@
+# fruits = ("apple", "banana", "cherry")
+# print(fruits)
+
+
+# nums = (10, 20, 30, 40, 50)
+# nums_list = list(nums)
+# print(nums_list)
+# print(type(nums_list))
+# print(f"index=",nums_list[0],"last=",nums_list[4])
+# nums = tuple(nums_list)
+# print(type(nums))
+
+#CONVERTING TUPLE INTO LIST .
+# CHANGE ITS MIDDLE ELEMENT THEN CHANGE IT BACK INTO TUPLE.
+# colors = ("red", "blue", "green")
+# my_colors=list(colors)
+# print(type(my_colors))
+# my_colors[1]="yellow"
+# print(my_colors)
+# colors=tuple(my_colors)
+# print(type(colors))
+
+#UNPACKING TUPLE
+# person=("Alice",25,"Engineer")
+# info_person=list(person)
+# print(type(info_person))
+# slice(info_person)
+# name="Name:",info_person[0]
+# print(name)
+# age="Age:",info_person[1]
+# print(age)
+# pro="profession:",info_person[2]
+# print(pro)
+
+animals = ("dog", "cat", "rabbit")
+for x in animals:
+    print(x)
+print(x)
