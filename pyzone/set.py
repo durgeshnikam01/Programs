f2d366494abbfce1977f5cf1e460b68a9fcb5aee
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-14 21:28:37 +0530

    set operations

--- pyzone/set.py
+++ pyzone/set.py
@@ -24,3 +24,85 @@
 # for x in nums:
 #     print(x+2,12)
 # print(x)
+
+# numbers = {1, 2, 2, 3, 4, 4, 5}
+# print(numbers)
+
+
+# my_list=[1,2,2,3,4,4,5,5,5]
+# new_list=set(my_list)
+# print(type(new_list))
+# print(new_list)
+# my_list=list(new_list)
+# print(type(my_list))
+
+# A = {1, 2, 3, 4, 5}
+# B = {4, 5, 6, 7, 8}
+# result=A.symmetric_difference(B)
+# print(result)
+
+# letters = {"a", "b", "c"}
+# letters.add("d")
+# letters.update("e","f")
+# print(letters)
+
+# nums = {1, 2, 3, 4, 5}
+# nums.remove(3) and nums.discard(6)
+# print(nums)
+
+# X= {1, 2}
+# Y= {1, 2, 3, 4}
+# print(X.issubset(Y))
+
+# data = {10, 20, 30, 40, 50}
+# data.pop()
+# print(data)
+
+# odd = {1, 3, 5, 7, 9}
+# even = {2, 4, 6, 8, 10}
+# result=odd.union(even)
+# print(result)
+
+
+# fruits = {"apple", "banana"}
+# fruits.add("cherry")
+# fruits.update(["mango", "orange"])
+# print(fruits)
+
+
+# nums = {1, 2, 3, 4}
+# nums.remove(3)   # removes 3
+# nums.discard(5)  # does nothing (no error even if 5 not in set)
+# print(nums)
+
+
+# A = {1, 2, 3}
+# B = {3, 4, 5}
+# print(A.union(B))
+
+
+# A = {1, 2, 3}
+# B = {2, 3, 4}
+# print(A & B)
+
+
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5}
+# print(A - B)   # elements in A but not in B
+# print(B - A)   # elements in B but not in A
+
+
+# A = {1, 2, 3, 4, 5}
+# B = {4, 5, 6, 7}
+# print(A ^ B)
+
+
+# X = {1, 2}
+# Y = {1, 2, 3, 4}
+# print(X.issubset(Y))   # True
+# print(Y.issuperset(X)) # True
+
+
+# numbers = [1, 2, 2, 3, 4, 4, 5]
+# unique = set(numbers)
+# print(unique)
