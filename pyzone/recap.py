23304eac2ede6b71b28e846bcf26b49f70cbedaa
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-18 10:59:59 +0530

    string and list operations

---
+++ pyzone/recap.py
@@ -0,0 +1,44 @@
+#STRING OPERATIONS
+
+# str="hello world"
+# print(str.upper())
+
+# str="python"
+# print(str[::-1])
+
+# str="banana"
+# print(str.count("a"))
+
+# str="madam"
+# if (str[::-1]==str):
+#     print("Yes palindrom")
+# else:print("NO")
+
+# str="apple,banana,cherry"
+# str_list=list(str)
+# print(str_list)
+
+#LIST OPERATIONS
+# x=[1, 2, 3, 4, 5]
+# x.append(6)
+# print(x)
+
+# x={1,2,3}
+# y={3,4,5}
+# z=x.union(y)
+# print(z)
+
+# x={10,20,30}
+# y={20,30,40}
+# z=x.intersection(y)
+# print(z)
+
+# x={1, 2, 3, 4}
+# (x.discard(3))
+# print(x)
+
+# x=[1, 2, 2, 3, 3, 4]
+# print(type(x))
+# z=set(x)
+# print(z,type(z))
+
