4ce403e79b104bbbf22a25ed0cb0750fa4b5cb7e
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-19 22:54:43 +0530

    conditional operations

---
+++ pyzone/coditions.py
@@ -0,0 +1,24 @@
+# x=int(input())
+# if x > 0:
+#     print("positive")
+# elif x<0:
+#     print("Negative")
+# else:print("zero")
+
+# marks = int(input("Enter your marks:"))
+# if marks >= 90 :
+#     print("A")
+# elif marks >= 75 and marks < 89:
+#     print("B")
+# elif marks >= 50 and marks < 74:
+#     print("C")
+# else:
+#     print("Fail")
+
+# num = int(input())
+# if num <=10:
+#     print("Small")
+# elif num > 10 and num <= 100:
+#     print("Medium")
+# else:
+#     print("Large")
