316d64cb3f5b7cbed4100c435a6e6a0a4c011be7
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-20 23:50:07 +0530

    loop operations

---
+++ pyzone/loop.py
@@ -0,0 +1,37 @@
+# x=1
+# while x < 101:
+#     print(x)
+#     x += 1
+
+# x=1
+# total=0
+# while x <= 20:
+#     total +=x
+#     x +=1
+
+# print("The sum of first 20 natural numbers is :",total)
+
+# num = int(input())
+# print(num(sum))
+# m = int(input("Enter a number: "))  # example: 123
+# s = 0
+
+# while m > 0:
+#     digit = m % 10     # get last digit
+#     s += digit         # add to sum
+#     m //= 10           # remove last digit
+
+# print("Sum of digits:", s)
+
+# Print digits of a number in reverse order
+# m = int(input("Enter a number: "))   # example: 1234
+
+# while m > 0:
+#     digit = m % 10      # take last digit
+#     print(digit, end=" ")
+#     m //= 10            # remove last digit
+
+
+# for x in range(1,101):
+#     print(x)
+    
