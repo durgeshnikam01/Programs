54238765375b4c23453f31b9c14f0e1d27f3804a
durgeshnikam01 <durgeshnikam42@gmail.com>
2025-09-22 11:11:04 +0530

    loop practice

---
+++ pyzone/loop_practice.py
@@ -0,0 +1,39 @@
+# nums = [2, 5, 8, 11, 14]
+# for x in nums:
+#     print(x)
+
+# marks = [45, 66, 72, 89, 54, 91]
+# for dx in marks:
+#     if dx % 2 == 0:
+#      print(dx)
+
+# nums = [10, 20, 30, 40]
+# for x in nums:
+#     print("The sum of list is :",sum(nums))
+#     break
+
+# names = ["Alice", "Bob", "Charlie", "David"]
+
+# for name in names:
+#     if name.startswith("C"):
+#         print(name)
+
+
+# nums=[1, 2, 3, 4, 5, 6]
+# for i in range(len(nums)):
+#     if nums[i] % 2 !=0:
+#         nums[i]= 0
+# print(nums)
+
+# str= ("python")
+# for i in str:
+#     print(i)
+
+# str="education"
+# ovel="aeiou"
+# count=0
+# for ch in str:
+#     if ch in ovel:
+#         count +=1
+# print("The number of vowels are :",count)
+    
